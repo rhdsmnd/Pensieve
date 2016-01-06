54448f685cd9dcc9e7c95b7ab8177a98e958b786
EC2 Default User <ec2-user@ip-172-31-18-250.us-west-2.compute.internal>
2016-01-06 12:25:53 +0000

    Changed to http server.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var http_port = 80
 
 app.get('/', function(req, res) {
     res.send('hello world');
 });
 
-console.log('server listening on port 3000');
-app.listen(3000);
+console.log('server listening on port ' + http_port);
+app.listen(http_port);

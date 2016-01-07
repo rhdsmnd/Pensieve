ba5eabcf1991306150da142d37c95c52eb3762c7
Ron Desmond <rhdsmnd@gmail.com>
2016-01-07 04:04:18 -0800

    Redirected http to https, and added self signed certificate.

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 var express = require('express');
-var app = express();
-var http_port = 80
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
 
-app.get('/', function(req, res) {
-    res.send('hello world');
+var privateKey = fs.readFileSync('./server.key', 'utf8');
+var certificate = fs.readFileSync('./server.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
+var express = require('express');
+var app = express();
+
+var https_port = 443;
+var http_port = 80;
+
+var httpServer = express();
+httpServer.get('*', function(req, res) {
+    res.redirect('https://127.0.0.1');
 });
 
+app.get('/', function(req, res) {
+    console.log(req.url);
+    res.send('hello world');
+});
+var httpsServer = https.createServer(credentials, app);
+
 console.log('server listening on port ' + http_port);
-app.listen(http_port);
+httpServer.listen(http_port);
+console.log('server listening on port ' + https_port);
+httpsServer.listen(https_port);
